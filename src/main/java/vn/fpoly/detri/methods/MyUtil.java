3674d86013abefa72d97e99f3131e0db26a3ebd1
Nguyen <emailcuaban@gmail.com>
2026-04-19 19:37:14 +0700

    testmyutiltot

--- src/main/java/vn/fpoly/detri/methods/MyUtil.java
+++ src/main/java/vn/fpoly/detri/methods/MyUtil.java
@@ -9,7 +9,7 @@ public class MyUtil {
         int tong= 0;
         for (int i = 0; i<= n; i++){
             if (i %2!=0){
-                tong += i+1;
+                tong += i;
             }
         }
         return tong;
